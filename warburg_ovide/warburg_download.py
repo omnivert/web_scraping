61ede5672e0a889434e80e050c51607d356e04e8
Giulio Santini <giulio.santini@gmail.com>
2020-10-31 17:26:37 -0400

    download all warburg html pages up til images / pyenv gitignore

--- warburg_ovide/warburg_download.py
+++ warburg_ovide/warburg_download.py
@@ -1,46 +1,84 @@
 import requests, csv
 from bs4 import BeautifulSoup as bsp
 from os import walk
+from pathlib import Path as pth
 
 ###### DOWNLOAD ALL REQUIRED HTML PAGES ######
 
-cycles_url = 'https://iconographic.warburg.sas.ac.uk/vpc/VPC_search/subcats.php?cat_1=8&cat_2=16&cat_3=1524&cat_4=2079'
+warburg_search_url = 'https://iconographic.warburg.sas.ac.uk/vpc/VPC_search/'
+ovide_cycles_suffix = 'subcats.php?cat_1=8&cat_2=16&cat_3=1524&cat_4=2079'
+#cycles_url = 'https://iconographic.warburg.sas.ac.uk/vpc/VPC_search/subcats.php?cat_1=8&cat_2=16&cat_3=1524&cat_4=2079'
+cycles_url = warburg_search_url + ovide_cycles_suffix
 # if the downloaded html page doesn't exist, save it
 cycles_fname = 'cycles.html'
 ## TODO check for existence of htmlpages dir
-htmlpages = []
 cwd = 'htmlpages'
+pth('./'+cwd).mkdir(parents=True, exist_ok=True)
+htmlpages = []
 for _, _, fnames in walk(cwd):
     htmlpages.extend(fnames)
 if cycles_fname not in htmlpages:
     page = requests.get(cycles_url)
     with open(cwd+'/'+cycles_fname, 'w') as f:
-        print('writing ' + cycles_fname + '...')
+        print('writing ' + cwd+'/'+cycles_fname + '...')
         f.write(page.text)
 else:
     print('file '+cwd+'/'+cycles_fname+' already exists')
-    
+# now parse all the cycles in the list:
+with open(cwd+'/'+cycles_fname) as f:
+    content = f.read()
+print('parsing cycle list...')
+soup = bsp(content, 'html.parser')
+cycle_list = soup.div.table.find_all_next('a')
+cycle_url_list = []
+for cycle_url in cycle_list:
+    cycle_full_url = warburg_search_url+cycle_url['href']
+    cycle_url_list.append(cycle_full_url)
+# now we move one dir down
+htmlpages_cyclesd = []
+cwd = 'htmlpages/cycles.d'
+pth('./'+cwd).mkdir(parents=True, exist_ok=True)
+# like above, make a list of what's already in the dir for checks
+for _, _, fnames in walk(cwd):
+    htmlpages_cyclesd.extend(fnames)
+# now for each cycle url name the html page the cat5 number in the url,
+# check if that file exists, if not download and save
+for c_url in cycle_url_list:
+    c_fname = 'cycle_'+c_url[111:]+'.html'
+    if c_fname not in htmlpages_cyclesd:
+        page = requests.get(c_url)
+        with open(cwd+'/'+c_fname, 'w') as f:
+            print('writing ' + cwd+'/'+c_fname + ' ...')
+            f.write(page.text)
+    else:
+        print('file '+cwd+'/'+c_fname+' already exists')
 
-'''
-urls = [url + '-{}'.format(x) for x in range(1312)]
-urls = [url] + urls
-
-urlmap = []
-
-# save all page info locally
-for i, u in enumerate(urls):
-    page = requests.get(u)
-    fname = 'url{}.html'.format(i)
-    with open('htmlpages/'+fname, 'w') as f:
-        print('writing ' + fname + '...')
-        f.write(page.text)
-    urlmap.append([fname, u])
-
-with open('urlmap.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    print('writing csv file...')
-    for row in urlmap:
-        csvwriter.writerow(row) 
-
-print('done')
-'''
+# now for the first large group download
+for c_url in cycle_url_list:
+    cwd = 'htmlpages/cycles.d'
+    with open(cwd+'/'+'cycle_'+c_url[111:]+'.html') as f:
+        c_content = f.read()
+    print('parsing cycle_{}.html...'.format(c_url[111:]))
+    sp = bsp(c_content, 'html.parser')
+    pg_list = sp.div.table.find_all_next('a')[1:]
+    pg_url_list = []
+    for pg in pg_list:
+        pg_url = warburg_search_url+pg['href']
+        pg_url_list.append(pg_url)
+    pg_url_list = list(set(pg_url_list[:]))
+    c_dname = 'cycle_'+c_url[111:]+'.d'
+    cwd = 'htmlpages/cycles.d/'+c_dname
+    pth('./'+cwd).mkdir(parents=True, exist_ok=True)
+    pg_htmlpages = []
+    for _, _, fnames in walk(cwd):
+        pg_htmlpages.extend(fnames)
+    # now at last, we've parsed things, time to download things
+    for pg_url in pg_url_list:
+        pg_fname = 'record_'+pg_url[72:]+'.html'
+        if pg_fname not in pg_htmlpages:
+            page = requests.get(pg_url)
+            with open(cwd+'/'+pg_fname, 'w') as f:
+                print('writing ' + cwd+'/'+pg_fname + ' ...')
+                f.write(page.text)
+        else:
+            print('file '+cwd+'/'+pg_fname+' already exists')

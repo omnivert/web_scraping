2999192195a2e4b71b421d8c3af30d37fa0d1613
Giulio Santini <giulio.santini@gmail.com>
2020-11-20 13:43:08 -0500

    tentative start at refactoring first part of process

--- warburg_ovide/warburg_download.py
+++ warburg_ovide/warburg_download.py
@@ -40,10 +40,52 @@ logger.addHandler(ch)
 #   parent (path on filesystem)
 
 class Node:
-    def __init__(self, data):
-        self.children = []
-        self.data = data
+    def __init__(self, children=None, parent=None, **kwargs):
+        self.children = children or []
+        self.parent = parent
+        self.data = kwargs
+        for child in self.children:
+            child.parent = self
 
+# takes a node, naming regex, and outputs proper filename
+# maybe expect namerules to be nameless function
+# then the text of this method is more like
+# node.data[path] + namerules(node.data[url])
+# TODO test this out, mb move to new file, treat this one as reference?
+def gen_filename(node, namerules):
+    print('gen_filename')
+    exit(0)
+
+# another string gen that takes a function
+# in this case we want to make a url
+def gen_leaf_url(node, namerules):
+    print('gen_leaf_url')
+    exit(0)
+
+# hmmmmmmm
+# i think i just need to think about this on paper
+# take site node
+#   contains url, html data as soup, cwd, "location" ie search terms to find leaves
+def find_leaves(node):
+    print('find_leaves')
+    exit(0)
+
+# mkdir -p parent dir
+# check if page present
+# if not, download
+def download_page(node):
+    cwd = node.data[cwd]
+    fname = node.data[fname]
+    pth('./{}'.format(cwd)).mkdir(parents=True, exist_ok=True)
+    htmlpages = []
+    for _, _, fnames in walk(cwd):
+        htmlpages.extend(fnames)
+    if fname not in htmlpages:
+        page = requests.get(node.data[url])
+        with open('./{}/{}'.format(cwd, fname), 'w') as f:
+            logger.debug('writing {}/{}...'.format(cwd, fname))
+            f.write(page.text)
+    
 ###### DOWNLOAD ALL REQUIRED HTML PAGES ######
 
 # so each time i'm downloading a bunch of stuff, what i'm actually doing is:
@@ -59,23 +101,10 @@ ovide_cycles_suffix = 'subcats.php?cat_1=8&cat_2=16&cat_3=1524&cat_4=2079'
 #cycles_url = 'https://iconographic.warburg.sas.ac.uk/vpc/VPC_search/subcats.php?cat_1=8&cat_2=16&cat_3=1524&cat_4=2079'
 cycles_url = warburg_search_url + ovide_cycles_suffix
 
-# takes a node, naming regex, and outputs proper filename
-# maybe expect namerules to be nameless function
-# then the text of this method is more like
-# node.data[path] + namerules(node.data[url])
-# TODO test this out, mb move to new file, treat this one as reference?
-def gen_filename(node, namerules):
-    print('gen_filename')
-    exit(0)
-
-# hmmmmmmm
-# i think i just need to think about this on paper
-# take site node
-#   contains url, html data as soup, cwd, "location" ie search terms to find leaves
-def find_leaves(node):
-    print('find_leaves')
-    exit(0)
-    
+cycles = Node([], url=cycles_url, fname='cycles.html', cwd='htmlpages')
+download_page(cycles)
+exit(0)
+# TODO TEST TEST TEST
 
 #warburg_vpc_url = 'https://iconographic.warburg.sas.ac.uk/vpc/'
 #warburg_search_url = warburg_vpc_url + 'VPC_search/'
